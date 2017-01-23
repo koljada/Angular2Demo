7d9898cb069e4ca8796f8f560e31a4581bbc0803
Roman <koljadar@gmail.com>
2017-01-23 22:12:54 +0200

    Configure ASP.NET Core backend. Add app.ts, npm, gulp

--- src/Angular2Demo/Startup.cs
+++ src/Angular2Demo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Angular2Demo
 {
@@ -47,7 +48,21 @@ namespace Angular2Demo
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
 
             app.UseMvc(routes =>
             {
