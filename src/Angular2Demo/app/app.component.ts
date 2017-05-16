78faf54b1ed1e89259588d72e524a2601c37bc23
Roman <roman.koliada@ssa-outsourcing.com>
2017-05-16 14:57:53 +0300

    Add some variables and localStorage

--- src/Angular2Demo/app/app.component.ts
+++ src/Angular2Demo/app/app.component.ts
@@ -2,9 +2,25 @@
 
 @Component({
     selector: "my-app",
-    template: "<button md-raised-button>Raised button</button><md-input-container>"+
-                "<input mdInput [mdDatepicker] = 'picker' placeholder='Choose a date'>"+
-                "<button mdSuffix [mdDatepickerToggle]='picker'></button>"+
-                "</md-input-container><md-datepicker #picker></md-datepicker>"
+    //template: "<button md-raised-button>Raised button</button><md-input-container>" +
+    //"<input mdInput [mdDatepicker] = 'picker' placeholder='Choose a date' [value]=startTime" +
+    //"<button mdSuffix [mdDatepickerToggle]='picker'></button>" +
+    //"</md-input-container><md-datepicker #picker></md-datepicker>",
+    template: "<h2>Start time: {{startTime | date: 'dd/MM/yyyy'}}</h2>"
 })
-export class AppComponent { }
+export class AppComponent {
+    private todayDate: string;
+    startTime: Date;
+
+    constructor() {
+        this.startTime = new Date();
+        this.todayDate = this.startTime.toDateString();
+        let todayDateRecord = localStorage.getItem(this.todayDate);
+        if (todayDateRecord == null) {
+            localStorage.setItem(this.todayDate, this.startTime.toString());
+        }
+        else {
+            this.startTime = new Date(Date.parse(todayDateRecord));
+        }
+    }
+}
